54368ceb2a1390edc2cb5a12b2347c4dad1fda78
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-01-18 14:55:09 -0800

    Break-1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className='header'>
+      <NavLink to="/" className='w-10 h-10 rounder-lg bg-white items-center justify-center flex font-bold shadow-md'>
+        <p className=''>KAT</p>
+      </NavLink>
+    </header>
   )
 }
 
