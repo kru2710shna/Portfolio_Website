5cda9aa37b5f4cf4ae5ae81244525a257ea004e0
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-01-19 15:55:30 -0800

    island created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,7 +37,6 @@ const Home = () => {
           <Suspense fallback={<Loader />}>
             <directionalLight position={[1,1,1]} intensity={2} />
             <ambientLight intensity={0.5} />
-            <spotLight />
             <hemisphereLight skyColor = '#b1e1ff' groundColor='#000000' intensity={1}/>
 
             <Bird />
