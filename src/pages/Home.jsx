a44c39c94a1126ef7fd62f033919491e52994e6f
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-01-20 15:22:15 -0800

    Bird Added and Working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,6 +48,7 @@ const Home = () => {
   
   return (
     <section className = "w-full h-screen relative">
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>POP-UP </div>
       <Canvas 
         className= {`w-full h-screen bg-transparent ${ isRotating ? 'cursor-grabbing' : 'cursor-grab' } `}
         camera={{near: 0.1, far: 1000}}
