7444498d372cd0f061f307ce4c2900f7a204b6ba
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-01-21 02:36:24 -0800

    Icons added for Skills Set

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+   <section
+   className='max-container'>
+    <h1 className='head=text'>
+      Hello I'm <span className='blue-gradient_text front-semibold
+      drop-shadow'>Krushna </span>
+    </h1>
+    <div
+    className='mt-5 flex flex-col gap-3 text-slate-500'>
+      <p>Enthusiast Machine Learning and Artificial Enginner, flattered on how 
+         Back-End developnment make so sense behind the scene.
+      </p>
+      </div>
+
+      <div className='py-10 flex flex-col'>
+        <h3 className='subhead-text'>
+          Tech-Stack
+        </h3>
+        <div className='mt-16 flex flex-wrap gap-12'>
+          
+        </div>
+      </div>
+   </section>
   )
 }
 
