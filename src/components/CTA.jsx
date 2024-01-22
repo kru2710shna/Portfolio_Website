7205eaac900d222280ff6339e4f4e638d3956207
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-01-22 01:50:06 -0800

    CTA created with footer

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const CTA = () => {
   return (
-    <div>CTA</div>
-  )
-}
+    <section className='cta'>
+      <p className='font-medium '>
+      As this chapter closes, a new one awaits. I'm actively seeking collaborations that challenge the status quo and redefine what's possible in the realm of technology and innovation. <br className='sm:block hidden' />
+      </p>
+      <Link to='/Contact' className='btn '>
+        Contact
+      </Link>
+    </section>
+  );
+};
 
-export default CTA
+export default CTA;
